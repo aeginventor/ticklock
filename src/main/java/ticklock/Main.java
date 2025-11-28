4e38fcf4c16495a8449638ef53b224197c18b222
San <aeginventor@gmail.com>
2025-11-28 23:57:56 +0900

    feat: 애플리케이션 실행 시 no-lock 시뮬레이션 수행

--- src/main/java/ticklock/Main.java
+++ src/main/java/ticklock/Main.java
@@ -1,8 +1,14 @@
 package ticklock;
 
+import ticklock.simulation.NoLockSimulation;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("ticklock – Step 1: Pure Java concurrency lab");
         System.out.println("온라인 티켓팅 동시성 실험을 시작합니다.");
+        System.out.println();
+
+        NoLockSimulation simulation = new NoLockSimulation();
+        simulation.run();
     }
 }
