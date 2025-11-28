13f1a0d8ac23fb86a51e0288db4552b5db1536a8
San <aeginventor@gmail.com>
2025-11-28 23:23:14 +0900

    build: java 21 툴체인과 application 플러그인 설정

---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    id("java")
+    application
+}
+
+group = "dev.ticklock"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+application {
+    mainClass.set("ticklock.Main")
+}
