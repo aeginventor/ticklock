5a5de2e53ac35541cc5a287c17cca24a981f6461
San <aeginventor@gmail.com>
2025-11-28 23:30:20 +0900

    feat: 프로젝트 실행을 위한 Main 클래스 추가

---
+++ src/main/java/ticklock/Main.java
@@ -0,0 +1,8 @@
+package ticklock;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("ticklock – Step 1: Pure Java concurrency lab");
+        System.out.println("온라인 티켓팅 동시성 실험을 시작합니다.");
+    }
+}
