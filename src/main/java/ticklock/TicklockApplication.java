bce2e6ee82f62194cac192fa19c315fdd8ca84e5
San <aeginventor@gmail.com>
2025-12-04 22:33:26 +0900

    feat: Spring Boot 애플리케이션 메인 클래스 생성

---
+++ src/main/java/ticklock/TicklockApplication.java
@@ -0,0 +1,13 @@
+package ticklock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicklockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicklockApplication.class, args);
+    }
+}
+
