03a333a962015f30c4dda5128f2853b835d17152
San <aeginventor@gmail.com>
2025-12-06 19:54:05 +0900

    feat: JPA 기반 티켓 구매 서비스 인터페이스 정의

---
+++ src/main/java/ticklock/service/jpa/JpaTicketPurchaseService.java
@@ -0,0 +1,16 @@
+package ticklock.service.jpa;
+
+/**
+ * JPA 기반 티켓 구매 서비스 인터페이스
+ */
+public interface JpaTicketPurchaseService {
+
+    /**
+     * 티켓을 구매합니다.
+     *
+     * @param eventId 이벤트 ID
+     * @return 구매 성공 시 true, 실패 시 false
+     */
+    boolean purchase(Long eventId);
+}
+
